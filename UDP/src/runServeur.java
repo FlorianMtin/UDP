7da9e5d6a9c9137d108789dd10f5b78990deaa11
FlorianMtin <florian.martin3@etu.univ-lyon1.fr>
2015-05-04 09:03:49 +0200

    UDP

---
+++ UDP/src/runServeur.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.ArrayList;
+
+
+public class runServeur {
+
+	public static void main(String[] args) throws IOException  {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> ls;
+		ls = ObjetConnect.ScanPort(1020,2040);
+		System.out.println(" Port utilisé " + ls.get(0));
+		
+		if (!ls.isEmpty()){
+		DatagramSocket ds = null;
+		try {
+			ds = new DatagramSocket(ls.get(0));
+		} catch (SocketException e) {
+			System.out.println("Aucun port dispo");
+		}
+		Serveur s = new Serveur(ds);
+		s.run();
+		}
+		
+		else{
+			System.out.println("Aucun port dispo");
+		}
+	}
+
+}
